5f79006df35fa982251ad02490c28a76f51b4d08
Kai Hwang <kai.hwang@gmail.com>
2024-05-31 10:51:48 -0500

    commit

--- lesion_mask.py
+++ lesion_mask.py
@@ -11,31 +11,40 @@ task_mask = nib.load('/home/kahwang/bkh/Lesions/task_0.5.nii.gz').get_fdata()
 
 list = pd.read_csv(masks_path+'list.txt', header=None, dtype='str')
 list = list.drop_duplicates()
-
 list = list.rename(columns={0:'subject'})
+list = list.reset_index()
+df = pd.read_csv('/home/kahwang/bkh/Lesions/Comparison_Potential.csv')
+tdf = pd.DataFrame()
+i=0
 for s in np.arange(len(list)):
     subject = list.loc[s,'subject']
-    mask = nib.load(masks_path + subject +".nii.gz")
-    lesion_mask = mask.get_fdata()>0
-    if np.sum(lesion_mask) < tha_size:
-        list.loc[s, 'smaller_than_thalamus'] = 'Yes'
-    else:
-        list.loc[s, 'smaller_than_thalamus'] = 'No'
-    list.loc[s, 'size'] = np.sum(lesion_mask) * 0.5*0.5*0.5
-    list.loc[s, 'task_contrast_mean'] = np.mean(task_mask[lesion_mask])
-    list.loc[s, 'task_contrast_sum'] = np.sum(task_mask[lesion_mask])
-    list.loc[s, 'wm_lesion_size'] = np.sum(wm_mask[lesion_mask]) * 0.5*0.5*0.5
-    list.loc[s, 'wm_percentage'] = list.loc[s, 'wm_lesion_size'] / list.loc[s, 'size']
-    if list.loc[s, 'task_contrast_mean'] > 0:
-        list.loc[s, 'group'] = 'positive'
-    else:
-        list.loc[s, 'group'] = 'negative'
+    if not np.any(df['subject'].values==int(subject)):
+        tdf.loc[i, 'subject'] = subject
+        mask = nib.load(masks_path + subject +".nii.gz")
+        lesion_mask = mask.get_fdata()>0
+        if np.sum(lesion_mask) < tha_size:
+            tdf.loc[i, 'smaller_than_thalamus'] = 'Yes'
+        else:
+            tdf.loc[i, 'smaller_than_thalamus'] = 'No'
+        tdf.loc[i, 'size'] = np.sum(lesion_mask) * 0.5*0.5*0.5
+        tdf.loc[i, 'task_contrast_mean'] = np.mean(task_mask[lesion_mask])
+        tdf.loc[i, 'task_contrast_sum'] = np.sum(task_mask[lesion_mask])
+        tdf.loc[i, 'wm_lesion_size'] = np.sum(wm_mask[lesion_mask]) * 0.5*0.5*0.5
+        tdf.loc[i, 'wm_percentage'] = tdf.loc[i, 'wm_lesion_size'] / tdf.loc[i, 'size']
+        if tdf.loc[i, 'task_contrast_mean'] > 0:
+            tdf.loc[i, 'group'] = 'positive'
+        else:
+            tdf.loc[i, 'group'] = 'negative'
+        i = i+1
 
-list.loc[(list['smaller_than_thalamus']=='Yes') & (list['wm_percentage'] < .4)& (list['task_contrast_mean']>0), 'control_group'] = 'positive'
-list.loc[(list['half_thalamus']=='Yes') & (list['wm_percentage'] < .4)& (list['task_contrast_mean']<0), 'control_group'] = 'negative'
-newList = list[list['control_group'].isna() == False]
-newList = newList.drop_duplicates()
-newList.to_csv('~/RDSS/tmp/find_patients.csv')
+tdf.loc[(tdf['smaller_than_thalamus']=='Yes') & (tdf['wm_percentage'] < 1)& (tdf['task_contrast_mean']>0), 'control_group'] = 'positive'
+tdf.loc[(tdf['smaller_than_thalamus']=='Yes') & (tdf['wm_percentage'] < 1)& (tdf['task_contrast_mean']<0), 'control_group'] = 'negative'
+newtdf = tdf[tdf['control_group'].isna() == False]
+newtdf = newtdf.drop_duplicates()
+df = df.append(newtdf)
+df.loc[df['review?']!='N'].to_csv('find_patients.csv')
+
+#df.to_csv('~/RDSS/tmp/find_patients.csv')
 
 
 #
